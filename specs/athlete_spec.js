f876d5fb8e1b3955b379440982406b2eebf8890b
Robert Ball <robert.ball.1989@gmail.com>
2017-04-04 13:46:36 +0100

    Made spec for athlete and test to check hydration.

---
+++ specs/athlete_spec.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var WaterBottle = require('../water_bottle.js');
+var Athlete = require('../athlete.js')
+
+describe('Athlete', function(){
+  it('should start with hydration at 100',function(){
+    var athlete = new Athlete();
+    assert.strictEqual(100,athlete.hydration);
+  })
+})
