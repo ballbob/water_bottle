b733f8474bf4412006a8128f058d15873d3a009f
Robert Ball <robert.ball.1989@gmail.com>
2017-04-04 14:29:37 +0100

    Water bottle modified for spacing.

--- water_bottle.js
+++ water_bottle.js
@@ -3,15 +3,18 @@ var WaterBottle = function(){
 }
 
 WaterBottle.prototype = {
+  
   fill: function(){
     this.volume = 100;
   },
+
   drink: function(){
     if (this.volume < 11){
       this.volume = 0;
     }else{
     this.volume = (this.volume-10);}
   },
+
   empty: function(){
     this.volume = 0;
   }
