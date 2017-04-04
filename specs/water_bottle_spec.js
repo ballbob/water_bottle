3c79f88a2eed7d3117d2e96f5559a2abf0009113
Robert Ball <robert.ball.1989@gmail.com>
2017-04-04 14:10:46 +0100

    Refactored water bottle spec to include beforeEach().

--- specs/water_bottle_spec.js
+++ specs/water_bottle_spec.js
@@ -2,34 +2,34 @@ var assert = require('assert');
 var WaterBottle = require('../water_bottle.js');
 
 describe('Water Bottle', function() {
+  var bottle;
+
+  beforeEach(function(){
+    bottle = new WaterBottle();
+  })
 
   it('should be empty at start', function(){
-    var bottle = new WaterBottle();
     assert.strictEqual(0,bottle.volume);
   })
 
   it('should fill when fill function used', function(){
-    var bottle = new WaterBottle();
     bottle.fill();
     assert.strictEqual(100,bottle.volume);
   })
 
   it('should go down by 10 when drink function used',function(){
-    var bottle = new WaterBottle();
     bottle.fill();
     bottle.drink();
     assert.strictEqual(90,bottle.volume);
   })
 
   it('should empty when empty function used',function(){
-    var bottle = new WaterBottle();
     bottle.fill();
     bottle.empty();
     assert.strictEqual(0,bottle.volume);
   })
 
   it('should not go below 0',function(){
-    var bottle = new WaterBottle();
     bottle.empty();
     bottle.drink();
     assert.strictEqual(0,bottle.volume);
