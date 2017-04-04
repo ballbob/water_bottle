a483e2c49a5d9f925bace920e7151a44ea03309d
Robert Ball <robert.ball.1989@gmail.com>
2017-04-04 14:07:40 +0100

    Changed spec file for athlete to include beforeEach(), initialising athletes and water bottles.

--- specs/athlete_spec.js
+++ specs/athlete_spec.js
@@ -3,23 +3,22 @@ var WaterBottle = require('../water_bottle.js');
 var Athlete = require('../athlete.js')
 
 describe('Athlete', function(){
-  it('should start with hydration at 100',function(){
 
+  beforeEach(function(){
+    var bottle = new WaterBottle();
     var athlete = new Athlete();
+  })
 
+  it('should start with hydration at 100',function(){
     assert.strictEqual(100,athlete.hydration);
   })
 
   it('should start having covered no distance', function(){
-
-    var athlete = new Athlete();
     assert.strictEqual(0,athlete.distanceCovered);
 
   })
 
   it('should run, decreasing hydration and increasing distance covered', function(){
-
-    var athlete = new Athlete();
     athlete.run(2);
 
     assert.strictEqual(98, athlete.hydration);
@@ -27,7 +26,6 @@ describe('Athlete', function(){
   })
 
   it('should not run any further if too dehydrated to travel that distance',function(){
-    var athlete = new Athlete();
     athlete.run(100);
     athlete.run(10);
 
@@ -35,15 +33,11 @@ describe('Athlete', function(){
   })
 
   it('should be able to drink the water bottle', function(){
-    
-    var athlete = new Athlete();
-    var bottle = new WaterBottle();
-
     athlete.run(100);
     athlete.drink(bottle);
 
     assert.strictEqual(100,athlete.hydration);
     assert.strictEqual(0,bottle.volume);
-
   })
+
 })
