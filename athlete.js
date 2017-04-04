063e5f6bbb84cea1e8bfdf2a77636911b8e9868b
Robert Ball <robert.ball.1989@gmail.com>
2017-04-04 13:51:17 +0100

    Athlete 'run' function increases distance covered and decreases hydration.

--- athlete.js
+++ athlete.js
@@ -1,6 +1,11 @@
 var Athlete = function(){
   this.hydration = 100;
   this.distanceCovered = 0;
+
+  this.run = function(distance){
+    this.distanceCovered = this.distanceCovered + distance;
+    this.hydration = this.hydration - distance;
+  }
 }
 
 module.exports = Athlete;
--- specs/athlete_spec.js
+++ specs/athlete_spec.js
@@ -4,12 +4,25 @@ var Athlete = require('../athlete.js')
 
 describe('Athlete', function(){
   it('should start with hydration at 100',function(){
+
     var athlete = new Athlete();
+    
     assert.strictEqual(100,athlete.hydration);
   })
 
   it('should start having covered no distance', function(){
+
     var athlete = new Athlete();
     assert.strictEqual(0,athlete.distanceCovered);
+
+  })
+
+  it('should run, decreasing hydration and increasing distance covered', function(){
+
+    var athlete = new Athlete();
+    athlete.run(2);
+
+    assert.strictEqual(98, athlete.hydration);
+    assert.strictEqual(2,athlete.distanceCovered);
   })
 })
